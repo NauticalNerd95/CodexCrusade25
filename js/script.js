25151bf84cca8f1894486bd3ab054060a58c341c
Shalom Salve <salveshalom838@gmail.com>
2025-07-06 20:33:58 +0530

    changed the redirection of first page

--- js/script.js
+++ js/script.js
@@ -59,5 +59,5 @@ function navigateWithFade(url) {
 
 // Use this function to navigate instead of direct navigation
 document.getElementById('nextButton').addEventListener('click', () => {
-    navigateWithFade('../pages/home.html');
+    navigateWithFade('../pages/calavera.html');
 });
